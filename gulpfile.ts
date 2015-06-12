a54ae6a4ab3f123796ac2424343fc79589449b89
whelmaze <whelmaze@gmail.com>
2015-06-12 20:30:40 +0900

    gulp: Output to single js

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,13 +3,19 @@
 
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 var tsProject = ts.createProject('./tsconfig.json');
 
 gulp.task('default', () => {
     var tsResult = tsProject.src()
+    	.pipe(sourcemaps.init())
 		.pipe(ts(tsProject));
 
-	return tsResult.js.pipe(gulp.dest('dest/js'));
+	return tsResult.js
+			.pipe(concat('output.js'))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('dest/js'));
 });
 

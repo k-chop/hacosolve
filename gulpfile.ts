36cfaa4698dd7ffd51224bcca12760808d968c91
whelmaze <whelmaze@gmail.com>
2015-07-04 16:25:51 +0900

    Add browserify gulp task

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,13 +1,23 @@
 /// <reference path="./src/typings/gulp/gulp.d.ts"/>
 
 import gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var shell = require('gulp-shell');
 var ts = require('gulp-typescript');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
 
 var tsProject = ts.createProject('./tsconfig.json');
 
 gulp.task('default', ['compile', 'test']);
+
+gulp.task('build', ['compile'], () => {
+    return browserify('src/ts/output.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dest/'));
+});
 
 gulp.task('compile', () => {
     var tsResult = gulp.src(['src/ts/*.ts',
@@ -19,7 +29,7 @@ gulp.task('compile', () => {
             .pipe(gulp.dest('src/ts'))
 			.pipe(concat('output.js'))
 			.pipe(sourcemaps.write())
-			.pipe(gulp.dest('dest/js'));
+			.pipe(gulp.dest('src/ts'));
 });
 
 gulp.task('test', ['compile'], () => {

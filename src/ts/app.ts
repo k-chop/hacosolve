41cf96b020ecd7d6b0bca6653f3410718ae619f1
kchop <whelmaze@gmail.com>
2020-10-06 23:19:51 +0900

    style

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,11 +6,16 @@ import { InitScene } from "./InitScene";
  * entry point
  */
 window.onload = () => {
+  document.body.style.margin = "5rem 0";
+  
   const app = new PIXI.Application(600, 600, {
     backgroundColor: 0x061639
   });
   const wrapper = document.createElement("div");
-  wrapper.style.textAlign = "center";
+  wrapper.style.display = "flex";
+  wrapper.style.justifyContent = "center";
+  wrapper.style.alignItems = "center";
+  
   document.body.appendChild(wrapper);
   wrapper.appendChild(app.view);
 

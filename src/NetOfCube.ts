38c47663a29e96c575c2131d77c188144c0c2fd6
k-chop <whelmaze@gmail.com>
2023-01-03 13:42:05 +0900

    生成する形状が分かりやすいように改行

--- src/NetOfCube.ts
+++ src/NetOfCube.ts
@@ -1,4 +1,4 @@
-import { Piece } from './Piece'
+import { Piece } from "./Piece"
 
 export class NetOfCube {
   public pieces: Piece[]
@@ -7,17 +7,52 @@ export class NetOfCube {
   public constructor() {
     this.pieces = new Array<Piece>(11)
 
-    this.pieces[0] = Piece.fromString('#   ' + '####' + '#   ', 4)
-    this.pieces[1] = Piece.fromString('#   ' + '####' + ' #  ', 4)
-    this.pieces[2] = Piece.fromString('#   ' + '####' + '  # ', 4)
-    this.pieces[3] = Piece.fromString('#   ' + '####' + '   #', 4)
-    this.pieces[4] = Piece.fromString(' #  ' + '####' + ' #  ', 4)
-    this.pieces[5] = Piece.fromString(' #  ' + '####' + '  # ', 4)
-    this.pieces[6] = Piece.fromString('###  ' + '  ###', 5)
-    this.pieces[7] = Piece.fromString('##  ' + ' ###' + ' #  ', 4)
-    this.pieces[8] = Piece.fromString('##  ' + ' ###' + '  # ', 4)
-    this.pieces[9] = Piece.fromString('##  ' + ' ###' + '   #', 4)
-    this.pieces[10] = Piece.fromString('##  ' + ' ## ' + '  ##', 4)
+    // prettier-ignore
+    {
+      this.pieces[0] = Piece.fromString(
+        "#   " +
+        "####" +
+        "#   ", 4)
+      this.pieces[1] = Piece.fromString(
+        "#   " +
+        "####" +
+        " #  ", 4)
+      this.pieces[2] = Piece.fromString(
+        "#   " +
+        "####" +
+        "  # ", 4)
+      this.pieces[3] = Piece.fromString(
+        "#   " +
+        "####" +
+        "   #", 4)
+      this.pieces[4] = Piece.fromString(
+        " #  " +
+        "####" +
+        " #  ", 4)
+      this.pieces[5] = Piece.fromString(
+        " #  " +
+        "####" +
+        "  # ", 4)
+      this.pieces[6] = Piece.fromString(
+        "###  " +
+        "  ###", 5)
+      this.pieces[7] = Piece.fromString(
+        "##  " +
+        " ###" +
+        " #  ", 4)
+      this.pieces[8] = Piece.fromString(
+        "##  " +
+        " ###" +
+        "  # ", 4)
+      this.pieces[9] = Piece.fromString(
+        "##  " +
+        " ###" +
+        "   #", 4)
+      this.pieces[10] = Piece.fromString(
+        "##  " +
+        " ## " +
+        "  ##", 4)
+    }
 
     this.all = this.allCandidates()
   }
